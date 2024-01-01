9764511f79b6326bac91e8901e85adc81762ee4d
mark mwendwa <mwendwamark310@gmail.com>
2024-01-01 15:18:06 +0300

    Organize file structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./Pages/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+        <Home/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import "../styles/Home.css"
+
+const Home = () => {
+  return (
+    <div>
+      <p>this is the home page</p>
+    </div>
+  )
+}
+
+export default Home
